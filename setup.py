44ae96b2f19c02413dc90b0073f615a4ccefc878
Simon Larsen <slarse@kth.se>
2017-05-27 14:42:54 +0200

    Add setup and requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pdfebc',
+    version='0.0.1',
+    description='Application to compress PDF documents into an ebook reader-friendly size.',
+    long_description=readme,
+    author='Simon Larsén',
+    author_email='slarse@kth.se',
+    url='https://github.com/slarse/pdfebc',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
