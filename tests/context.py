3b98753b35afaec78f9f5fab7a797aeba4059a50
Simon Larsen <slarse@kth.se>
2017-05-31 21:31:38 +0200

    Add utils module to tests context

--- tests/context.py
+++ tests/context.py
@@ -2,4 +2,4 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import pdfebc.core, pdfebc.cli
+import pdfebc.core, pdfebc.cli, pdfebc.utils
