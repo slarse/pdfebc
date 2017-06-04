416a82daa7be1f83bf914da84e745d1efca7b2af
Simon Larsen <slarse@kth.se>
2017-06-04 13:33:17 +0200

    Refactor main to use cli.status_callback

--- pdfebc/main.py
+++ pdfebc/main.py
@@ -13,7 +13,8 @@ def main():
     args = parser.parse_args()
     if not args.nomakedir:
         os.makedirs(args.outdir)
-    filepaths = core.compress_multiple_pdfs(args.sourcedir, args.outdir, args.ghostscript)
+    filepaths = core.compress_multiple_pdfs(args.sourcedir, args.outdir,
+                                            args.ghostscript, cli.status_callback)
     if args.send:
         if not utils.valid_config_exists():
             user, password, reciever = cli.prompt_for_config_values()
