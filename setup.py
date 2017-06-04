319c3161dc8b2ea25e3d3cdb081dfe36df6da65c
Simon Larsen <slarse@kth.se>
2017-06-04 21:31:11 +0200

    Increment PATCH version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ required = ['appdirs']
 
 setup(
     name='pdfebc',
-    version='0.1.0',
+    version='0.1.1',
     description='Application to compress PDF documents into an ebook reader-friendly size.',
     long_description=readme,
     author='Simon Larsén',
