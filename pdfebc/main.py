4b65c71021396c135ad597cd4c578c03123a409c
Simon Larsen <slarse@kth.se>
2017-06-07 19:38:52 +0200

    Add error handling to send call in main

--- pdfebc/main.py
+++ pdfebc/main.py
@@ -5,7 +5,22 @@ Author: Simon Larsén
 """
 import os
 import shutil
+import smtplib
 from . import cli, core, utils
+
+AUTH_ERROR = """An authentication error has occured!
+Status code: {}
+Message: {}
+
+This usually happens due to incorrect username and/or password in the configuration file, 
+so please look it over!"""
+
+UNEXPECTED_ERROR = """An unexpected error occurred when attempting to send the e-mail.
+
+Python error repr: {}
+
+Please open an issue about this error at 'https://github.com/slarse/pdfebc/issues'.
+"""
 
 def main():
     """Run PDFEBC."""
@@ -20,7 +35,12 @@ def main():
             user, password, receiver = cli.prompt_for_config_values()
             config = utils.create_email_config(user, password, receiver)
             utils.write_config(config)
-        utils.send_files_preconf(filepaths, status_callback=cli.status_callback)
+        try:
+            utils.send_files_preconf(filepaths, status_callback=cli.status_callback)
+        except smtplib.SMTPAuthenticationError as e:
+            cli.status_callback(AUTH_ERROR.format(e.smtp_code, e.smtp_error))
+        except Exception as e:
+            cli.status_callback(UNEXPECTED_ERROR.format(repr(e)))
     if args.clean:
         shutil.rmtree(args.outdir)
 
